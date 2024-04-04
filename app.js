0ba96ed7a6eea3678cb61d64f7d2520cdfe8da06
Reed Wigley <rwigley55@gmail.com>
2024-04-04 11:57:16 -0700

    add show page route, pass api data to show page

--- app.js
+++ app.js
@@ -23,11 +23,33 @@ const fetchCountries = async () => {
   }
 };
 
+const fetchCountry = async (name = "") => {
+  try {
+    const response = await axios.get(
+      `https://restcountries.com/v3.1/name/${name}?fullText=true`
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
+
 // Home route
 app.get("/", async (req, res) => {
   const countryData = await fetchCountries();
 
   res.render("home", { countryData: countryData });
+});
+
+// Show route
+app.get("/:name", async (req, res) => {
+  // console.log(req.params);
+  const { name } = req.params;
+  const countryData = await fetchCountry(name);
+
+  res.render("show", { country: countryData[0] });
 });
 
 app.listen(3000, () => {
