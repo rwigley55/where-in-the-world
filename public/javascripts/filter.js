92f02208666fad7df24dfd0d224660d56fa30c49
Reed Wigley <rwigley55@gmail.com>
2024-04-15 16:30:45 -0700

    add filter functionality

---
+++ public/javascripts/filter.js
@@ -0,0 +1,16 @@
+const regionFilter = document.querySelector("#region-filter");
+// const cards = document.querySelectorAll(".country-box");
+
+regionFilter.addEventListener("change", () => {
+  const selectedRegion = regionFilter.value.toLowerCase();
+
+  cards.forEach((card) => {
+    const countryRegion = card
+      .querySelector(".country-region")
+      .textContent.toLowerCase();
+
+    const isRegionVisible =
+      selectedRegion === "" || countryRegion.includes(selectedRegion);
+    card.classList.toggle("hide", !isRegionVisible);
+  });
+});
